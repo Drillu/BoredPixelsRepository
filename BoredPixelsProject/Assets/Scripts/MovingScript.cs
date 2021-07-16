8f2d8294f437f1d58e2047f2063b7ece3319a836
Drillu <maxmax98765@yahoo.ro>
2021-07-16 14:41:27 +0300

    Prototype

---
+++ BoredPixelsProject/Assets/Scripts/MovingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Vector2 movement = Vector2.zero;
+    public float moveSpeed=5f;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (movement != Vector2.zero)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        }
+        
+    }
+}
