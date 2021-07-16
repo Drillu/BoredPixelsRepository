02cd4a29d0d4bd3acad064484839bfb57df4bbd2
Nadir-Alishly <nadir@alishly.com>
2021-07-16 22:34:13 +0400

    player controls changed

---
+++ BoredPixelsProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController controller;
+    public float speed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
