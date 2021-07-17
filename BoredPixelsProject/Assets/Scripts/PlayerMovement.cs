5b3453b1edcecc313df6f2ba4b22595dc219f39e
Nadir-Alishly <nadir@alishly.com>
2021-07-17 22:07:38 +0400

    player animations added

--- BoredPixelsProject/Assets/Scripts/PlayerMovement.cs
+++ BoredPixelsProject/Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public PlayerController controller;
+    public Animator animator;
     public float speed = 40f;
 
     float horizontalMove = 0f;
+    bool walking = false;
     bool jump = false;
     bool crouch = false;
     
     void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+        if(Input.GetAxisRaw("Horizontal")!=0)
+        {
+            walking = true;
+            animator.SetBool("isWalking",true);
+        }
+        else
+        {
+            walking = false;
+            animator.SetBool("isWalking",false);
+        }
 
         if(Input.GetButtonDown("Jump"))
         {
