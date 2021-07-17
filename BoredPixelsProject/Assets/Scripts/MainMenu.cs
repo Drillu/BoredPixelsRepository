69f7e9607570561fec80e735d3c70cccb5e30243
Drillu <maxmax98765@yahoo.ro>
2021-07-17 11:08:19 +0300

    transition

--- BoredPixelsProject/Assets/Scripts/MainMenu.cs
+++ BoredPixelsProject/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    Animator animator;
+    public float transTime = 1f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void PlayButton()
     {
+       StartCoroutine(LoadLevel());
+    }
+
+    public IEnumerator LoadLevel()
+    {
+        animator.SetTrigger("Trans");
+        yield return new WaitForSeconds(transTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
