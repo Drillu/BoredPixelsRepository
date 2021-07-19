f29b13fd7569072691c14997b2c2ce35536e837e
Nadir-Alishly <nadir@alishly.com>
2021-07-19 12:58:20 +0400

    delivery truck added

---
+++ BoredPixelsProject/Assets/Scripts/Interactible.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+public class Interactible : MonoBehaviour
+{
+    public GameObject message;
+    public float transTime = 1f;
+
+    public Vector3 messageOffset;
+    public UnityEvent Interact;
+
+    Animator animator;
+
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        message.transform.position = Camera.main.WorldToScreenPoint(transform.position + messageOffset);   
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            message.SetActive(true);
+            if(Input.GetButtonDown("Interact"))
+            {
+                Interact.Invoke();
+            }
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            message.SetActive(false);
+        }
+    }
+
+    public void SleepInBox()
+    {
+        StartCoroutine(SleepInBoxCoroutine());
+    }
+
+    public IEnumerator SleepInBoxCoroutine()
+    {
+        animator.SetTrigger("Trans");
+        yield return new WaitForSeconds(transTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
