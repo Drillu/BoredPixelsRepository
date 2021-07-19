22e829e6c42580ba3e58f7b6be1b3c35706a1953
Nadir-Alishly <nadir@alishly.com>
2021-07-19 22:38:09 +0400

    house tiles added

---
+++ BoredPixelsProject/Assets/Scripts/Openable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openable : MonoBehaviour
+{
+    public Sprite openedSprite;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = openedSprite;
+        gameObject.GetComponent<Collider2D>().enabled = false;
+    }
+}
