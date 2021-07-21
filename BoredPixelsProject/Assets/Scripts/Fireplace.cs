cf80bb6d9650afd5f6c5b191aa9823c7e22d7ae5
Nadir-Alishly <nadir@alishly.com>
2021-07-21 20:08:32 +0400

    roof tops location added

---
+++ BoredPixelsProject/Assets/Scripts/Fireplace.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplace : MonoBehaviour
+{
+    public float onTime;
+    public float offTime;
+    public PlayerMovement player;
+    public ParticleSystem fire;
+
+    private bool isOn;
+
+    void Start()
+    {
+        isOn = false;
+        fire.Stop();
+        StartCoroutine(TurnOn());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "fireDetector" && isOn)
+        {
+            player.gameOver = true;
+        }
+    }
+
+    IEnumerator TurnOn()
+    {
+        yield return new WaitForSeconds(onTime);
+        fire.Play();
+        isOn = true;
+        StartCoroutine(TurnOff());
+    }
+
+    IEnumerator TurnOff()
+    {
+        yield return new WaitForSeconds(offTime);
+        fire.Stop();
+        isOn = false;
+        StartCoroutine(TurnOn());
+    }
+
+
+}
